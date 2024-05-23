9d30f35147639a656a74ea8d9b37abc1d72f7c91
Dena Shiranipour <denashirani@gmail.com>
2024-05-23 16:52:18 +0100

    created the other tables, need to do last mvp point,correct one

--- safari_park.sql
+++ safari_park.sql
@@ -1,8 +1,15 @@
-DROP TABLE IF EXISTS enclosure;
-DROP TABLE IF EXISTS animal;
-DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS assignment;
+DROP TABLE assignment;
+DROP TABLE enclosure;
+DROP TABLE animal;
+DROP TABLE employee;
 -- does enclosure need to come first?
+
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    employee_number INT
+);
 
 CREATE TABLE enclosure (
     id SERIAL PRIMARY KEY,
@@ -20,12 +27,6 @@ CREATE TABLE animal (
 );
 
 
-CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    employee_number INT
-);
-
 CREATE TABLE assignment (
     id SERIAL PRIMARY KEY,
     employee_id INT REFERENCES employee(id),
@@ -33,6 +34,10 @@ CREATE TABLE assignment (
     day VARCHAR(255)
 );
 
+
+INSERT INTO employee (name, employee_number) VALUES ('Mario', 1);
+INSERT INTO employee (name, employee_number) VALUES ('Peach', 2);
+INSERT INTO employee (name, employee_number) VALUES ('Toad', 3);
 
 INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Plain', 400, false);
 INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('River', 70, false);
@@ -46,11 +51,11 @@ INSERT INTO animal (name, type, age, enclosure_id) VALUES ('Flamingo', 'Bird', 3
 INSERT INTO animal (name, type, age, enclosure_id) VALUES ('Hippo', 'Large Semiaquatic Mammal', 23, 3);
 INSERT INTO animal (name, type, age, enclosure_id) VALUES ('Fennec Fox', 'Small Mammal', 1, 1);
 
-
-INSERT INTO employee (name, employee_number) VALUES ('Mario', 1);
-INSERT INTO employee (name, employee_number) VALUES ('Peach', 2);
-INSERT INTO employee (name, employee_number) VALUES ('Toad', 3);
-
 INSERT INTO assignment (employee_id, enclosure_id) VALUES (1, 2);
 INSERT INTO assignment (employee_id, enclosure_id) VALUES (2, 1);
 INSERT INTO assignment (employee_id, enclosure_id) VALUES (3, 3);
+
+
+
+-- MVP 1:
+SELECT animal.name, enclosure.name FROM animal INNER JOIN enclosure ON animal.enclosure_id = enclosure.id;
